3f8ad713b3f323623fe41433dfacedcf04cc5453
Dmitrii Sukharev <sdm831@gmail.com>
2021-11-16 23:40:47 +0300

    first step

---
+++ FamilyQuest/Controllers/WelcomeController.cs
@@ -0,0 +1,17 @@
+﻿using FamilyQuest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyQuest.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Login(User user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ FamilyQuest/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyQuest.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+
+        public User()
+        {
+            Id = Guid.NewGuid();            
+        }
+    }
+}
